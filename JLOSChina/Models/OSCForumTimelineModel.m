3d4b2120fbfcef5ec14c5cfeafdd6ac50f218d97
jimneylee <jimneylee@gmail.com>
2014-08-11 15:07:57 +0800

    change path call method

--- JLOSChina/Models/OSCForumTimelineModel.m
+++ JLOSChina/Models/OSCForumTimelineModel.m
@@ -33,14 +33,37 @@
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-- (NSString*)relativePath
+- (NSString *)relativePath
 {
     NSString* path = nil;
-    path = [OSCAPIClient relativePathForForumListWithForumType:self.topicType
-                                                   pageIndex:self.pageIndex
-                                                  pageSize:self.pageSize];
+    path = [OSCAPIClient relativePathForForumListWithCatalogId:[self getCatalogIdFromType:self.topicType]
+                                                     pageIndex:self.pageIndex
+                                                      pageSize:self.pageSize];
     return path;
 
+}
+
+- (NSString *)getCatalogIdFromType:(OSCForumTopicType)type
+{
+    NSString *catalogId = nil;
+    switch (type) {
+        case OSCForumTopicType_All:// = 0,      //所有
+        case OSCForumTopicType_QA:// = 1,       //问答
+        case OSCForumTopicType_Share:// = 2,    //分享
+        case OSCForumTopicType_Watering:// = 3, //综合灌水
+        case OSCForumTopicType_Feedback:// = 4:   //站务
+            catalogId = [NSString stringWithFormat:@"%d", type];
+            break;
+            
+        case OSCForumTopicType_Career:// = 100,   //职业
+            catalogId = @"100";
+            break;
+            
+        default:
+            break;
+    }
+    
+    return catalogId;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
