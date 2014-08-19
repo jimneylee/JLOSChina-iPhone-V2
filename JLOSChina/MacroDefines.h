0bc70126c2435008486cecca8139a6b29ea499fc
jimneylee <jimneylee@gmail.com>
2014-08-19 13:28:43 +0800

    change detail font size

--- JLOSChina/MacroDefines.h
+++ JLOSChina/MacroDefines.h
@@ -83,7 +83,7 @@
 //#define CATALOG_TWEET 3
 
 // News Content HTML, idea from old version
-#define HTML_Style @"<style>#oschina_title {color: #000000; margin-bottom: 6px; font-weight:bold;}#oschina_title img{vertical-align:middle;margin-right:6px;}#oschina_title a{color:#0D6DA8;}#oschina_outline {color: #707070; font-size: 15px;}#oschina_outline a{color:#0D6DA8;}#oschina_software{color:#808080;font-size:15px}#oschina_body img {max-width: 290px;}#oschina_body {font-size:18px;max-width:290px;line-height:24px;} #oschina_body table{max-width:290px;}#oschina_body pre { font-size:12pt;font-family:Courier New,Arial;border:1px solid #ddd;border-left:5px solid #6CE26C;background:#f6f6f6;padding:5px;}</style>"
+#define HTML_Style @"<style>#oschina_title {color: #000000; margin-bottom: 6px; font-weight:bold;}#oschina_title img{vertical-align:middle;margin-right:6px;}#oschina_title a{color:#0D6DA8;}#oschina_outline {color: #707070; font-size: 15px;}#oschina_outline a{color:#0D6DA8;}#oschina_software{color:#808080;font-size:15px}#oschina_body img {max-width: 290px;}#oschina_body {font-size:16px;max-width:290px;line-height:24px;} #oschina_body table{max-width:290px;}#oschina_body pre { font-size:12pt;font-family:Courier New,Arial;border:1px solid #ddd;border-left:5px solid #6CE26C;background:#f6f6f6;padding:5px;}</style>"
 #define HTML_Bottom @"<div style='margin-bottom:20px'/>"
 
 #endif
