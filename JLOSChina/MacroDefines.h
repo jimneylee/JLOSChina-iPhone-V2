720ae5413da365afec8e51a1aa865d8a42ad8807
jimneylee <jimneylee@gmail.com>
2014-08-22 16:13:30 +0800

    add APP_THEME_BLUE_COLOR

--- JLOSChina/MacroDefines.h
+++ JLOSChina/MacroDefines.h
@@ -52,6 +52,7 @@
 
 // Color配色
 #define APP_THEME_COLOR RGBCOLOR(26, 144, 56)//(41, 41, 41)
+#define APP_THEME_BLUE_COLOR RGBCOLOR(27, 128, 219)
 #define APP_NAME_GREEN_COLOR RGBCOLOR(85, 201, 66)
 #define APP_NAME_WHITE_COLOR RGBCOLOR(200, 200, 200)
 #define TABLE_VIEW_BG_COLOR RGBCOLOR(237, 234, 234)
@@ -77,13 +78,8 @@
 #define DID_LOGOUT_NOTIFICATION @"DID_LOGOUT_NOTIFICATION"
 #define DID_POST_NEW_TWEET_SUCCESS_NOTIFICATION @"DID_POST_NEW_TWEET_SUCCESS_NOTIFICATION"
 
-// 内容类型：资讯、博客、帖子、动弹（微博）类型
-//#define CATALOG_NEWS 1
-//#define CATALOG_FORUM 2
-//#define CATALOG_TWEET 3
-
 // News Content HTML, idea from old version
-#define HTML_Style @"<style>#oschina_title {color: #000000; margin-bottom: 6px; font-weight:bold;}#oschina_title img{vertical-align:middle;margin-right:6px;}#oschina_title a{color:#0D6DA8;}#oschina_outline {color: #707070; font-size: 15px;}#oschina_outline a{color:#0D6DA8;}#oschina_software{color:#808080;font-size:15px}#oschina_body img {max-width: 290px;}#oschina_body {font-size:16px;max-width:290px;line-height:24px;} #oschina_body table{max-width:290px;}#oschina_body pre { font-size:12pt;font-family:Courier New,Arial;border:1px solid #ddd;border-left:5px solid #6CE26C;background:#f6f6f6;padding:5px;}</style>"
-#define HTML_Bottom @"<div style='margin-bottom:20px'/>"
+#define HTML_STYLE @"<style>#oschina_title {color: #000000; margin-bottom: 6px; font-weight:bold;}#oschina_title img{vertical-align:middle;margin-right:6px;}#oschina_title a{color:#0D6DA8;}#oschina_outline {color: #707070; font-size: 15px;}#oschina_outline a{color:#0D6DA8;}#oschina_software{color:#808080;font-size:15px}#oschina_body img {max-width: 290px;}#oschina_body {font-size:16px;max-width:290px;line-height:24px;} #oschina_body table{max-width:290px;}#oschina_body pre { font-size:12pt;font-family:Courier New,Arial;border:1px solid #ddd;border-left:5px solid #6CE26C;background:#f6f6f6;padding:5px;}</style>"
+#define HTML_BOTTOM @"<div style='margin-bottom:20px'/>"
 
 #endif
